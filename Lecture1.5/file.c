0a445d0c9d9888543793cf91bf83f6f518bfe081
Max Nova <MaxNova.CompSci@gmail.com>
2019-02-22 03:03:40 -0800

    read lec 1 and 1.5

--- Lecture1.5/file.c
+++ Lecture1.5/file.c
@@ -6,3 +6,11 @@ int main()
     system("ls /tmp/file.dat");
     return 0;
 }
+
+//notes
+/*
+If you can alter environment variables you can redirect system
+calls to search in directories of your choosing
+put your own binary in there renamed to something its look for and you
+can redirect the program execution from ls to l337 hack
+*/
